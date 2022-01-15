4154a6c4f9676b861572bbbd86653306f48d3d9e
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2022-01-15 12:16:20 +0100

    get random origin destination pairs

--- spawn_despawn_points.py
+++ spawn_despawn_points.py
@@ -11,6 +11,7 @@ import geopandas as gpd
 import pandas as pd
 from os import path
 import numpy as np
+import random
 
 def main():
     # import airspace polygon with geopandas
@@ -18,20 +19,64 @@ def main():
     airspace = gpd.read_file(airspace_path, driver='GPKG', layer='total_polygon')
 
     # get origin and destination points for rogue aircraft
-    origin_gdf, destination_gdf = get_spawn_despawn(airspace, 64, 100)
+    spawn_gdf, despawn_gdf = get_spawn_despawn_gdfs(airspace, 64, 100, 12000)
 
-    # select minimum distance between points in km
-    min_dist = 12 
-    # select a random orign and destination point
-    origin = origin_gdf.sample(1)
-    destination = destination_gdf.sample(1)
+    # get 10 origin and destination pairs
+    origin_destination_pairs = get_n_origin_destination_pairs(spawn_gdf, despawn_gdf, 10)
 
-    print(origin)
-def get_spawn_despawn(airspace, buffer_distance=64, spawn_distance=100):
+def get_n_origin_destination_pairs(spawn_gdf, despawn_gdf, n):
+    """
+    Takes the spawn and despawn points and returns a list of n  
+    randomly generated origin and destination pairs.
+
+    Parameters
+    ----------
+    spawn_gdf : geopandas.GeoDataFrame
+        GeoDataFrame with origin points containing a column with a list of
+        destination points that are in the despawn gdf.
+
+    despawn_gdf : geopandas.GeoDataFrame
+        GeoDataFrame with destination points.
+
+    n : int
+        Number of origin and destination pairs to return.
+        
+    Returns
+    -------
+    origin_destination_pairs : list
+        List of n randomly generated origin and destination pairs.
+    """
+
+    # get the number of origin and destination points
+    origin_destination_pairs = []
+    
+    for _ in range(n):
+        
+        # select a random origin
+        origin_gdf = spawn_gdf.sample(1)
+        origin_location = origin_gdf['geometry'].values[0]
+
+        # select a random destination from the destination list
+        destination_list = origin_gdf['valid_destinations'].values[0]
+        destination_idx = random.choice(destination_list)
+
+        # get the destination point from despawn gdf
+        destination_location = despawn_gdf.loc[destination_idx].values[0]
+
+        # add to list
+        origin_destination_pairs.append((origin_location, destination_location))
+    
+    return origin_destination_pairs
+
+def get_spawn_despawn_gdfs(airspace, buffer_distance=64, spawn_distance=100, min_path_distance=12000):
     """
     Takes the total airspace border and offsets it by a certain amount and
     then takes the resulting polygon and splits it into a set of points that
-    that are spaced by a certain distance. The resulting points can then be used
+    that are spaced by a certain distance. Also there is a column in the 
+    origin gdf that gives a list of the destination points further than a
+    minimum distance.
+    
+    The resulting points can then be used
     as origin and destination points for rogue aircraft.
     
     Parameters
@@ -45,10 +90,14 @@ def get_spawn_despawn(airspace, buffer_distance=64, spawn_distance=100):
     spawn_distance : int
         Distance between each spawn point.
 
+    min_path_distance : int
+        Minimum distance between each potential spawn/despawn point.
+
     Returns
     -------
     origin_gdf : geopandas.GeoDataFrame
-        GeoDataFrame with origin points.
+        GeoDataFrame with origin points. Also contains a column with a list of
+        destination points further than a minimum distance (valid_destinations).
 
     destination_gdf : geopandas.GeoDataFrame
         GeoDataFrame with destination points.
@@ -74,6 +123,21 @@ def get_spawn_despawn(airspace, buffer_distance=64, spawn_distance=100):
     # and put the coordinates ina geodataframe
     origin_gdf = gpd.GeoDataFrame(geometry=rogue_points[::2], crs=airspace.crs)
     destination_gdf = gpd.GeoDataFrame(geometry=rogue_points[1::2], crs=airspace.crs)
+
+    # get the distance between each origin and destination point
+    valid_destinations = []
+    for _, row in origin_gdf.iterrows():
+        
+        # find potential destination points when distance is greater than minimum
+        potential_destinations = destination_gdf.loc[
+            (destination_gdf['geometry'].distance(row['geometry']) > min_path_distance)
+            ].index.tolist()
+        
+        # append to a list of valid destinations for a particular origin
+        valid_destinations.append(potential_destinations)
+
+    # add the list of valid destinations to the origin gdf
+    origin_gdf['valid_destinations'] = valid_destinations
 
     return origin_gdf, destination_gdf
 
