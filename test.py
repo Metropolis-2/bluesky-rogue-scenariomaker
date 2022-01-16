86a541137b49b56b2d98b4112d515cb250533d44
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2022-01-16 13:20:48 +0100

    update get rogue path

--- test.py
+++ test.py
@@ -1,17 +1,27 @@
+#%%
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.spatial.transform import Rotation as R
 
 # find angle with horizontal axis from start to finish using atan2
-angle = np.deg2rad(179)
+angle = np.deg2rad(30)
+
+# find the start point of path
+start_vector = np.array([-10,-10])
 
 # create random noise
-x = np.random.uniform(0, 1, 100)
 x = np.linspace(0, 10, 10)
 y =  np.zeros(len(x))
 z = np.zeros(len(x))
-xy = np.vstack((x, y, z)).T
 
+# add noise
+noise = np.random.uniform(low=-4, high=4, size=len(x))
+y = y + noise
+# first and last value of y should be 0
+y[0], y[-1] = 0, 0
+
+# create the signal
+xy = np.vstack((x, y, z)).T
 
 # find the quadrant
 if angle > np.pi/2:
@@ -28,11 +38,11 @@ else:
 
 # create a rotation matrix
 r = R.from_euler('xyz', [0, 0, angle], degrees=False)
-print(np.rad2deg(angle))
-print(quadrant)
+
 # rotate the points
 xy_rot = r.apply(xy)
 
+# reflect the points
 if quadrant == 2:
     # reflect vector about y-axis
     xy_ref = np.vstack((-xy_rot[:,0], xy_rot[:,1])).T
@@ -49,6 +59,8 @@ elif quadrant == 4:
 else:
     xy_ref = xy_rot[:,:2]
 
+# translate the points with start vector
+xy_trans = xy_ref + start_vector
 
 # into a plot
 
@@ -60,8 +72,9 @@ plt.plot(x_axis,0*y_axis, 'k--')
 plt.plot(0*x_axis,y_axis, 'k--')
 
 plt.plot(xy[:,0], xy[:,1], '-')
-plt.plot(xy_ref[:,0], xy_ref[:,1], '--')
-plt.xlim([-15, 15])
-plt.ylim([-15, 15])
+plt.plot(xy_trans[:,0], xy_trans[:,1], '--')
+plt.xlim([-20, 20])
+plt.ylim([-20, 20])
 
 plt.show()
+# %%
